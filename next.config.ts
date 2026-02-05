36f838a833a7b61cc087ab9393b48ff995786da1
AlexKosha <ovelikoshich@gmail.com>
2026-02-05 16:19:07 +0100

    export

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
   basePath: isProd ? "/smm-portfolio" : "",
   assetPrefix: isProd ? "/smm-portfolio/" : "",
 
